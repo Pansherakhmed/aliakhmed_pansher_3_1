6c7956a7b644a9b602e1bbcfe246bb37345ed842
Pansherakhmed <aliakhmedd@icloud.com>
2022-09-07 23:07:50 +0600

    nums

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        double[] nums = {124, 333, 456, -222, -930, 133, 356, -456, -567, 179, -444, 580, 23, -23, 55};
+        double[] nums = {8,-2,-4, 2, 3, 6, 35, -46, -57, 79, -44, 80, 3, -2, 5};
         double result = 0;
         boolean negative = false;
         for (double hi : nums) {
